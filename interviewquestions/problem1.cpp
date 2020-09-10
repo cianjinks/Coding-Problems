3cff35138784ce7c890c20b011e636d33659dab8
cianjinks <cjinks99@gmail.com>
2020-09-10 23:06:48 +0100

    Initial git

---
+++ interviewquestions/problem1.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::vector<int> nums = {0, 1, 2, 3};
+    s.removeDuplicates(nums);
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
