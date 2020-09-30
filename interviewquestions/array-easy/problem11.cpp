869ecf739f50b4452fdfe4565cd79103b4e0f371
cianjinks <cjinks99@gmail.com>
2020-09-30 23:02:20 +0100

    Refactored folder layout and did easy strings problem 1

---
+++ interviewquestions/array-easy/problem11.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/770/
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+class Solution {
+public:
+    void rotate(std::vector<std::vector<int>>& matrix) {
+        // Lets try just rotating the outer ring
+        int n = (int)matrix[0].size();
+        for(int x = 0; x < n; x++)
+        {
+            for(int y = 0; y < n; y++)
+            {
+                
+            }
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
